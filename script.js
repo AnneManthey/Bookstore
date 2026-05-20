f695935731a71f223c2a27720bf1c811645593f5
Anne Manthey <anne.manthey@pm.me>
2026-05-20 12:00:27 +0200

    update standard.css

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 
-
 function init(){
-    
     renderBooks();
 }
 
@@ -11,3 +9,14 @@ function renderBooks(){
     bookRef.innerHTML += getBooksTemplate();
 }
 
+
+// Variablen in html template
+// for-loop anlegen & JSON rendern lassen
+// Like-Funktion: Onclick Button Bild Ändern + Likes +1/-1
+// Kommentar-Funktion: Text aus Input auslesen und als Kommentar hinzufügen
+
+// Bonus: Local Storage
+// Bonus: Favoriten markieren & anzeigen lassen
+
+// Optional: Buch im Dialog-Fenster öffnen lassen
+// 
