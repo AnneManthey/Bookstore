16052c826605bc31da6f2705b482e8490e640736
Anne Manthey <anne.manthey@pm.me>
2026-05-21 09:00:34 +0200

    update templates

--- scripts/template.js
+++ scripts/template.js
@@ -3,14 +3,14 @@ function getBooksTemplate(index){
         <article class="book_card">
             <header class="book_header">
                 <h2 id="book_title">${bookList[index].name}</h2>
-                <div class="book_header_cover"><img src="./assets/icons/book_icon.svg" alt="Book cover"></div> 
+                <div class="book_header_cover"><img src="./assets/icons/book_icon.svg" alt="Book cover"></div>
             </header>
             <section class="book_body">
                 <div class="book_body_price">
                     <p id="book_price">00,00€</p>
                     <div class="book_likes">
                         <p id="book_likes">${bookList[index].likes}</p>
-                        <button id="like_button" class="like_button"><img src="./assets/icons/like_outline.svg"alt="like icon"></button>
+                        <button id="like_button" class="like_button"><img src="./assets/icons/like_outline.svg" alt="like icon"></button>
                     </div>
                 </div>
                 <table class="book_table">
@@ -28,23 +28,15 @@ function getBooksTemplate(index){
                     </tr>
                 </table>
             </section>
-            <section >
-                <div class="book_comment_section">
-                    <h3>Kommentare:</h3>
+            <section class="book_comment_section">
+                <h3>Kommentare:</h3>
                 <table id="comment_section" class="book_table">
-                    <!-- <tr>
-                        <th>[Username]</th>
-                        <td id="book_comments">Kommentar</td>
-                    </tr> -->
-                    
                 </table>
-                </div>
-                <footer class="book_footer">
-                    <input type="text" id="comment_input" class="comment_input" name="comment"
-                    placeholder="Schreibe einen Kommentar ..."><br><br>
-                <button class="comment_button"><img src="./assets/icons/send_icon.png" alt="send_icon"></button>
-                </footer>
             </section>
+            <footer class="book_footer">
+                <input type="text" id="comment_input" class="comment_input" name="comment" placeholder="Schreibe einen Kommentar ..."><br><br>
+                <button class="comment_button"><img src="./assets/icons/send_icon.png" alt="send_icon"></button>
+            </footer>
         </article>
     `
 }
@@ -54,7 +46,7 @@ function getCommentsTemplate(index, commentIndex){
             <tr>
                 <th>${bookList[index].comments[commentIndex].name}</th>
                 <td id="book_comments">${bookList[index].comments[commentIndex].comment}</td>
-            </tr>
+            </tr>   
     `
 
 }
